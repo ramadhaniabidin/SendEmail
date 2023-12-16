b8864921d52aa475f8c518341d79f59f4864b6a7
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-12-16 18:36:57 +0700

    Initial Commit

---
+++ SendEmail/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string senderEmail = "ramadhaniabidin12@gmail.com";
+        string senderPassword = "lopg gggw mnrj xwrs";
+
+        MailMessage message = new MailMessage();
+        message.From = new MailAddress(senderEmail);
+        message.Subject = "Testing send second email";
+        message.To.Add(new MailAddress("my.second.ramadhaniabidin12@gmail.com"));
+        message.Body = "<html><body> Test Body </body></html>";
+        message.IsBodyHtml = true;
+
+        var smtpClient = new SmtpClient("smtp.gmail.com");
+        smtpClient.Port = 587;
+        smtpClient.Credentials = new NetworkCredential(senderEmail, senderPassword);
+        smtpClient.EnableSsl = true;
+        smtpClient.Send(message);
+    }
+}
